1fce6f3f342a1c539124828b7e14fc2f838847b3
Yunjee Heo <heoyunjee@gmail.com>
2017-11-21 21:17:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { NativeModules, requireNativeComponent, View, Text } from 'react-native';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import {
+  NativeModules,
+  requireNativeComponent,
+  View,
+  Text,
+  LayoutAnimation,
+} from "react-native";
 
 const { RNChannel } = NativeModules;
+const { UIManager } = NativeModules;
 
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
 const defaultStyles = {
   channel: {
     flex: 0,
     position: "absolute",
-    width: 96,
-    height: 96,
+    width: 330,
+    height: 500,
     right: 0,
     bottom: 4,
   },
@@ -20,10 +29,10 @@ const initialStyles = {
   channel: {
     flex: 0,
     position: "absolute",
-    width: 96,
-    height: 95,
+    width: 330,
+    height: 501,
     right: 0,
-    bottom: 5,
+    bottom: 4,
   },
 };
 
@@ -32,41 +41,44 @@ class ChannelViewComponent extends Component {
     super(props);
     this.state = {
       update: false,
-    }
+    };
   }
 
   componentDidMount() {
-    const timoutId = setinterval(() => {
+    const timoutId = setInterval(() => {
       this.setState({
         update: !this.state.update,
       });
     }, 1000);
     this.setState({ timoutId });
   }
-  
+
   componentWillUnmount() {
-   clearInterval(this.state.timoutId);
+    clearInterval(this.state.timoutId);
   }
 
   render() {
+    // LayoutAnimation.spring();
     return (
-        <ChannelView
-          accessibilityLabel={'Button'}
-          accessibilityComponentType={'button'}
-          accessible={true}
-          importantForAccessibility={'yes'}
-          style={(this.state.update) ? initialStyles.channel : defaultStyles.channel}
-          {...this.props}
-        />
+      <ChannelView
+        accessibilityLabel={"Button"}
+        accessibilityComponentType={"button"}
+        accessible
+        importantForAccessibility={"yes"}
+        style={
+          this.state.update ? initialStyles.channel : defaultStyles.channel
+        }
+        {...this.props}
+      />
     );
   }
 }
 
 ChannelViewComponent.propTypes = {
   ...View.propTypes,
-}
+};
 
-const ChannelView = requireNativeComponent('ChannelView', ChannelViewComponent);
+const ChannelView = requireNativeComponent("ChannelView", ChannelViewComponent);
 
 export default RNChannel;
 export { ChannelViewComponent as ChannelView };
